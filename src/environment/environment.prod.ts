8c4143e56f20a02579c06a154dd6bb1e83a3f615
AbdulKareem Afolabi <dgenafo@gmail.com>
2024-10-04 13:25:01 -0700

    update url

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-  //   apiUrl: 'https://api.example.com',  // Use your production API URL
+    // apiUrl: 'https://api.example.com',  // Use your production API URL
   };
