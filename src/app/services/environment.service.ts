bdd935e4571df2ac46b396831250d4edd6064d67
AbdulKareem Afolabi <dgenafo@gmail.com>
2024-10-06 04:39:18 -0700

    updated modal

--- src/app/services/environment.service.ts
+++ src/app/services/environment.service.ts
@@ -21,7 +21,7 @@ export class EnvironmentService {
     return this.http.post<any>(`${this.baseUrl}dashboard.php`, obj, { withCredentials: true })
   }
   public userRequestPassword(obj:any){
-    return this.http.post<any>(`${this.baseUrl}sendemail.php`, obj)
+    return this.http.post<any>(`${this.baseUrl}mail.php`, obj)
   }
   public userResetPassword(obj:any){
     return this.http.post<any>(`${this.baseUrl}resetpassword.php`, obj)
